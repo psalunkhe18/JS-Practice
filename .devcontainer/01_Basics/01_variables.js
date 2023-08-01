e7332ea629080ca8af4194c04785a5c6d027ddc9
psalunkhe18 <123489781+psalunkhe18@users.noreply.github.com>
2023-08-01 09:18:57 +0000

    datatypes and variables

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "gita@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+accountEmail = "rita@gmail.com"
+ accountPassword = "54321"
+accountCity = "Pune"
+
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ .devcontainer/01_Basics/02_Datatypes.js
@@ -0,0 +1,10 @@
+let name = ""
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 56
+// bigInt
+// string => ""
+// boolean => true/false (user either logged or not)
+// null => standalone value (representation of empty value)
+// undefined => value is not defined
